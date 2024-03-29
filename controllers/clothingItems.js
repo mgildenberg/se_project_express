e98a412252edfda7931b735365169eb0bfb7dfd3
mgildenberg <pulsesync@gmail.com>
2024-03-29 11:46:22 -0700

    wip: more error handling and bugfix

--- controllers/clothingItems.js
+++ controllers/clothingItems.js
@@ -10,7 +10,7 @@ const ForbiddenError = require("../errors/ForbiddenError");
 const ConflictError = require("../errors/ConflictError");
 const UnauthorizedError = require("../errors/UnauthorizedError");
 
-const getClothingItems = (req, res) => {
+const getClothingItems = (req, res, next) => {
   ClothingItem.find({}) // would return all the clothingItems
     .then((clothingItems) => res.send({ clothingItems }))
     .catch((err) => {
@@ -19,40 +19,7 @@ const getClothingItems = (req, res) => {
     });
 };
 
-// used this to test
-// const getClothingItemById = (req, res) => {
-//   const clothingItemId = req.params.itemId;
-
-//   ClothingItem.findById(clothingItemId)
-//     .orFail(() => {
-//       const error = new Error(
-//         ` No item found with the given ID: ${clothingItemId}`,
-//       );
-//       error.status = 404;
-//       error.name = "ItemNotFoundError";
-//       throw error;
-//     })
-//     .then((clothingItem) => {
-//       res.send({ clothingItem });
-//     })
-//     .catch((err) => {
-//       console.error(err);
-//       console.log(err.name);
-//       if (err.name === "ItemNotFoundError") {
-//         return res.status(err.status).send({ message: err.message });
-//       }
-//       if (err.name === "CastError") {
-//         return res
-//           .status(VALIDATION_ERROR)
-//           .send({ message: `${err.name} | ID did not match expected format` });
-//       }
-//       return res
-//         .status(INTERNAL_SERVER_ERROR)
-//         .send({ message: "An error has occurred on the server." });
-//     });
-// };
-
-const createClothingItem = (req, res) => {
+const createClothingItem = (req, res, next) => {
   const { name, weather, imageUrl } = req.body;
   const userId = req.user._id;
   ClothingItem.create({
@@ -69,29 +36,43 @@ const createClothingItem = (req, res) => {
       console.log(err.name);
       if (err.name === "ValidationError") {
         next(new BadRequestError("Incorrect or invalid data"));
-        // return res
-        //   .status(VALIDATION_ERROR)
-        //   .send({ message: `${err.name} | ${err.message}` });
       } else {
         next(err);
       }
-      // return res
-      //   .status(INTERNAL_SERVER_ERROR)
-      //   .send({ message: "An error has occurred on the server." });
     });
 };
 
-const deleteClothingItem = (req, res) => {
+// used this to test
+const getClothingItemById = (req, res, next) => {
   const clothingItemId = req.params.itemId;
 
   ClothingItem.findById(clothingItemId)
     .orFail(() => {
-      // const noIdFoundError = new Error(
-      //   `No item found with the given ID: ${req.params.itemId}`,
-      // );
-      // noIdFoundError.name = "NoIdFoundError";
-      // noIdFoundError.status = 404;
-      // throw noIdFoundError;
+      throw new NotFoundError(
+        ` No item found with the given ID: ${clothingItemId}`,
+      );
+    })
+    .then((clothingItem) => {
+      res.send({ clothingItem });
+    })
+    .catch((err) => {
+      console.error(err);
+      console.log(err.name);
+      // if (err.name === "ItemNotFoundError") {
+      //   return res.status(err.status).send({ message: err.message });
+      // }
+      if (err.name === "CastError") {
+        next(new BadRequestError("Incorrect or invalid data"));
+      }
+      next(err);
+    });
+};
+
+const deleteClothingItem = (req, res, next) => {
+  const clothingItemId = req.params.itemId;
+
+  ClothingItem.findById(clothingItemId)
+    .orFail(() => {
       throw new NotFoundError(
         `No item found with the given ID: ${req.params.itemId}`,
       );
@@ -102,29 +83,16 @@ const deleteClothingItem = (req, res) => {
           res.send({ message: "Item deleted" });
         });
       }
-      // const wrongUserError = new Error("Cannot delete another user's item");
-      // wrongUserError.name = "WrongUserError";
-      // wrongUserError.status = 403;
-      // throw wrongUserError;
-      throw new (ForbiddenError("Cannot delete another user's item"))();
+      throw new ForbiddenError("Cannot delete another user's item");
     })
     .catch((err) => {
       console.error(err);
       if (err.name === "CastError") {
-        return res
-          .status(VALIDATION_ERROR)
-          .send({ message: `${err.name} | ID did not match expected format` });
+        next(
+          new BadRequestError(`${err.name} | ID did not match expected format`),
+        );
       }
-      // if (err.name === "WrongUserError") {
-      //   return res.status(err.status).send({ message: err.name + err.message });
-      // }
-      // if (err.name === "NoIdFoundError") {
-      //   return res.status(err.status).send({ message: err.name + err.message });
-      // }
       next(err);
-      // return res
-      //   .status(INTERNAL_SERVER_ERROR)
-      //   .send({ message: "An error has occurred on the server." });
     });
 };
 
@@ -132,4 +100,5 @@ module.exports = {
   getClothingItems,
   createClothingItem,
   deleteClothingItem,
+  getClothingItemById,
 };
